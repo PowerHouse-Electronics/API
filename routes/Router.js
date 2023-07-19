dccfb1ed608b88a74faad28732f29b5a40465f42
ulisesVela16 <uli.vela16@gmail.com>
2023-07-19 07:04:58 -0600

    rutaActualizada

--- routes/Router.js
+++ routes/Router.js
@@ -14,8 +14,8 @@ router.get('/users/:secret', usersController.getUsers);
 //Rutas celulares 
 router.get('/cellphones', cellController.getAllCellPhones);
 router.post('/cellphones', cellController.addCellPhone);
-router.put('/cellphones/:brand/:model', cellController.updateCellPhone);
-router.delete('/cellphones/:brand/:model', cellController.deleteCellPhone);
+router.put('/cellphones/:id', cellController.updateCellPhone);
+router.delete('/cellphones/:id', cellController.deleteCellPhone);
 
 //Rutas computadoras 
 router.get('/cpus', cpuController.getAllCPUs);
